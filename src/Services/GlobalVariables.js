e87c13c2440eeff64edb4c4a1ccea903a973d20a
antony <antony.jacques06@gmail.com>
2021-12-31 15:20:19 +0100

    setup config file

---
+++ src/Services/GlobalVariables.js
@@ -0,0 +1,10 @@
+const URL_API_STAT = 'http://localhost:3000'
+
+const userId = 18
+
+const requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+}
+
+export { URL_API_STAT, requestOptions, userId }
---
+++ src/Services/RequestAPI.js
@@ -0,0 +1,7 @@
+import { URL_API_STAT } from './GlobalVariables'
+
+export default function RequestAPI() {
+    const urlAPI = URL_API_STAT
+
+    console.log(urlAPI)
+}
